fa745fdcf3f7a6b45679618468f930e1bd0dc94e
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-11-03 09:53:53 +0530

    Solved exercise

---
+++ Week 2/week2ex1/week2ex1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace week2ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n;
+            double.TryParse(textBox1.Text, out n);
+            textBox2.Text = (n * 2).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text = " ";
+        }
+    }
+}
