c71580b7d3ba85b23ec1842b497b6d634d917719
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-11-04 10:21:37 +0530

    question 2

---
+++ Week 3/question_2/question_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace question_2
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public static double Cost { get; set; }
+    }
+    class Program
+    {
+        static double calcGST()
+        {
+            return 0.08 * Item.Cost;
+        }
+        static void Main(string[] args)
+        {
+            Item i = new Item();
+            i.Name = "Nutella";
+            Item.Cost = 200;
+            Console.WriteLine("GST = " + calcGST());
+        }
+    }
+}
