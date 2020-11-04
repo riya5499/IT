302addb7a0bb144ed17f40e8fc0e64f64f3012e7
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-11-04 10:38:11 +0530

    question 3

---
+++ Week 3/question_3/question_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace question_3
+{
+    class TrafficSignal
+    {
+        public void Red ()
+        {
+            Console.WriteLine("RED -> STOP");
+        }
+
+        public void Yellow ()
+        {
+            Console.WriteLine("YELLOW -> BE READY");
+        }
+
+        public void Green ()
+        {
+            Console.WriteLine("GREEN -> GO");
+        }
+    }
+    class Program
+    {
+        public delegate void TrafficDel();
+        static void Main(string[] args)
+        {
+            TrafficSignal t = new TrafficSignal();
+            TrafficDel tred = t.Red;
+            TrafficDel tyellow = t.Yellow;
+            TrafficDel tgreen = t.Green;
+            tred(); tyellow(); tgreen();
+        }
+    }
+}
