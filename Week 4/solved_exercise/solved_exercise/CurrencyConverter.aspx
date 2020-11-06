d610eb5b15b0c2dbf189133d98f86cf8d9a35b2f
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-11-06 08:53:15 +0530

    solved exercise

---
+++ Week 4/solved_exercise/solved_exercise/CurrencyConverter.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CurrencyConverter.aspx.cs" Inherits="solved_exercise.CurrencyConverter" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" id="Label1">Input Currency:</asp:Label>
+            <asp:TextBox runat="server" id="inputCurrency"/>
+            <asp:Label id="fromLable" runat="server" >From USD to:</asp:Label>
+            <asp:DropDownList id="fromCurrencyDropDown" runat="server"/>
+            <br />
+            <br />
+            <asp:Label runat="server" id="labelAns" />
+            <br />
+            <br />
+            <asp:Button Text="Convert!" OnClick="SubmitClickedEvent" runat="server"/>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week 4/solved_exercise/solved_exercise/CurrencyConverter.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace solved_exercise
+{
+    public partial class CurrencyConverter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                fromCurrencyDropDown.Items.Add(new ListItem("INR", "74.14"));
+                fromCurrencyDropDown.Items.Add(new ListItem("EURO", "0.84"));
+                fromCurrencyDropDown.Items.Add(new ListItem("YEN", "103.48"));
+
+            }
+        }
+
+        protected void SubmitClickedEvent (object sender, EventArgs e)
+        {
+            string convertedValue = ConvertCurrency();
+            labelAns.Text = convertedValue;
+        }
+
+        public string ConvertCurrency ()
+        {
+            double old_value = 0;
+            double new_value = 0;
+            double.TryParse(inputCurrency.Text, out old_value);
+            ListItem temp = fromCurrencyDropDown.Items[fromCurrencyDropDown.SelectedIndex];
+            new_value = old_value * double.Parse(temp.Value);
+            string s = old_value.ToString() + " USD = " + new_value.ToString() + " " + temp.Text;
+            return s;
+        }
+    }
+}
