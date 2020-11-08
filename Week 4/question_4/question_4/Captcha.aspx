1dc267ba313470ca9e3655261a120a2ad9e5be23
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-11-08 15:35:06 +0530

    question 4

---
+++ Week 4/question_4/question_4/Captcha.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Captcha.aspx.cs" Inherits="question_4.Captcha" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" ID="captcha"></asp:Label>
+            <br />
+            <br />
+            <asp:Label runat="server" ID="enterLabel">Enter here:</asp:Label>
+            <asp:TextBox runat="server" ID="enter"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button runat="server" ID="checkBtn" OnClick="Check" Text="Check" />
+            <br />
+            <br />
+            <asp:Label runat="server" ID="answerLabel"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week 4/question_4/question_4/Captcha.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace question_4
+{
+    public partial class Captcha : System.Web.UI.Page
+    {
+        public static int count;
+        public static string captchaText;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                captchaText = getCaptcha();
+                captcha.Text = "Captcha: " + captchaText;
+                count = 3;
+                answerLabel.Text = "You have 3 attempts remaining";
+            }
+        }
+
+        public string getCaptcha()
+        {
+            string characters = "012345679ACEFGHKLMNPRSWXZabcdefghijkhlmnopqrstuvwxyz";
+            System.Text.StringBuilder captcha = new System.Text.StringBuilder();
+            Random r = new Random();
+            int i;
+            for(i=0;i<6;i++)
+            {
+                captcha.Append(characters[r.Next(characters.Length)]);
+            }
+            return captcha.ToString();
+        }
+
+        public void Check(object sender, EventArgs e)
+        {
+            count--;
+            if(count == 0)
+            {
+                if (enter.Text == captchaText)
+                {
+                    answerLabel.Text = "Success!";
+                }
+                else
+                {
+                    enter.Enabled = false;
+                    answerLabel.Text = "Oops! You have utilised all your 3 attempts";
+                }
+            }
+            else
+            {
+                if (enter.Text == captchaText)
+                {
+                    answerLabel.Text = "Success!";
+                }
+                else
+                {
+                    answerLabel.Text = "You have " + count + " attempts remaining";
+                }
+            }  
+        }
+    }
+}
