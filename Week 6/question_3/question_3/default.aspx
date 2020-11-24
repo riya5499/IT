8fd27af4df4fac927be229b98f322079bce068bd
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-11-24 15:51:56 +0530

    question 3

---
+++ Week 6/question_3/question_3/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="question_3._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" ID="lbl1">Choose Theme: </asp:Label>
+            <asp:DropDownList runat="server" ID="themeList" AutoPostBack="true" OnSelectedIndexChanged="ThemeChange"></asp:DropDownList>
+            <br />
+            <asp:Image runat="server" ID="img" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week 6/question_3/question_3/default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace question_3
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                themeList.Items.Add("");
+                themeList.Items.Add("Summer");
+                themeList.Items.Add("Monsoon");
+
+                if(Session["theme"] != null)
+                {
+                    themeList.Items.FindByText(Session["theme"].ToString()).Selected = true;
+                }
+            }
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if(Session["theme"] != null)
+            {
+                Page.Theme = Session["theme"].ToString();
+            }
+        }
+
+        protected void ThemeChange(object sender, EventArgs e)
+        {
+            Session["theme"] = themeList.SelectedValue;
+            Server.Transfer(Request.FilePath);
+        }
+    }
+}
