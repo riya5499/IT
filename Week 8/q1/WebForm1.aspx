62288e9884e811f087033fce5a7e76dc999418bf
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-12-07 00:57:48 +0530

    Add files via upload

---
+++ Week 8/q1/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="q1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            JUICE SHOP
+            <br />
+            <hr />
+            <br />
+            Select Fruit : <asp:CheckBoxList runat="server" ID="fruitList"></asp:CheckBoxList>
+            <br />
+            <br />
+            Select Ice Cream : <asp:RadioButtonList runat="server" ID="icecreamList"></asp:RadioButtonList>
+            <br />
+            <br />
+            <asp:Button runat="server" ID="btn" Text="Select above options" OnClick="SelectAction" />
+            <br />
+            <br />
+            <asp:Label runat="server" ID="lbl"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week 8/q1/WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace q1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                List<string> fruits = new List<string>();
+                fruits.Add("Strawberry");
+                fruits.Add("Blueberry");
+                fruits.Add("Mango");
+                fruits.Add("Kiwi");
+                fruitList.DataSource = fruits;
+
+                List<string> icecream = new List<string>();
+                icecream.Add("Chocolate");
+                icecream.Add("Cookie n Cream");
+                icecream.Add("ButterScotch");
+                icecream.Add("Vanilla");
+                icecreamList.DataSource = icecream;
+
+                this.DataBind();
+            }
+        }
+
+        protected void SelectAction(object sender, EventArgs e)
+        {
+            int price = 0;
+            foreach(ListItem li in fruitList.Items)
+            {
+                if(li.Selected)
+                {
+                    switch(li.Value)
+                    {
+                        case "Strawberry": price += 50;break;
+                        case "Blueberry": price += 60; break;
+                        case "Mango": price += 60; break;
+                        case "Kiwi": price += 65; break;
+                    }
+                }
+            }
+            switch(icecreamList.SelectedValue)
+            {
+                case "Chocolate": price += 50; break;
+                case "Cookie n Cream": price += 60; break;
+                case "ButterScotch": price += 50; break;
+                case "Vanilla": price += 45; break;
+            }
+
+            lbl.Text = "Milkshake : ";
+            foreach(ListItem li in fruitList.Items)
+            {
+                if(li.Selected)
+                {
+                    lbl.Text += li.Value + " + ";
+                }
+            }
+            lbl.Text += "(icecream)" + icecreamList.SelectedValue + "<br />" + "Price : " + price;
+    }
+}
