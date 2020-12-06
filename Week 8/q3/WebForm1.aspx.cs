c6dd6292924488ec4ebf4bde7497c5d387618f36
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-12-07 01:02:44 +0530

    Add files via upload

---
+++ Week 8/q3/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace q2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SelectAction(object sender, EventArgs e)
+        {
+            string url = "WebForm2.aspx?";
+            url += "Place=" + packageList.SelectedValue;
+            Response.Redirect(url);
+        }
+    }
+}
