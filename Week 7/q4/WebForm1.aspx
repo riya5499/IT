558b5dcc97335cd92d292af6f388a7cbbdb34e81
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-12-07 00:55:56 +0530

    Add files via upload

---
+++ Week 7/q4/WebForm1.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="q4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div id="left" style="width:50%; float:left;height:400px">
+                <br />
+                ADD EMPLOYEE
+                <br />
+                <hr />
+                <br />
+                Employee Name : <asp:TextBox runat="server" ID="empName"></asp:TextBox>
+                <br />
+                <br />
+                Company Name : <asp:TextBox runat="server" ID="companyName"></asp:TextBox>
+                <br />
+                <br />
+                Salary : <asp:TextBox runat="server" ID="salary"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Button runat="server" ID="insertEmployee" Text="Add Employee" OnClick="AddEmployeeAction" />
+                <br />
+                <br />
+                <asp:Label runat="server" ID="lbl1"></asp:Label>
+            </div>
+            <div id="right" style="width:50%; float:right; height:400px">
+                <br />
+                FIND EMPLOYEE
+                <br />
+                <hr />
+                <br />
+                Company Name : <asp:TextBox runat="server" ID="findCompany"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Button runat="server" ID="find" Text="Find Employees" OnClick="FindEmployeeAction" />
+                <br />
+                <br />
+                <asp:Label runat="server" ID="lbl2"></asp:Label>
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week 7/q4/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace q4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string connectionString = WebConfigurationManager.ConnectionStrings["Employee"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AddEmployeeAction(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("INSERT INTO WORKS VALUES (@n, @c, @s)", con);
+            command.Parameters.AddWithValue("@n", empName.Text);
+            command.Parameters.AddWithValue("@c", companyName.Text);
+            command.Parameters.AddWithValue("@s", salary.Text);
+            con.Open();
+            command.ExecuteNonQuery();
+            lbl1.Text = "Successfully inserted!!";
+            empName.Text = companyName.Text = salary.Text = "";
+            con.Close();
+        }
+
+        protected void FindEmployeeAction(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("Select w.empName, l.city from WORKS w, LIVES l where w.companyName = @c AND w.empName = l.empName", con);
+            command.Parameters.AddWithValue("@c", findCompany.Text);
+            con.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                lbl2.Text = "Name : " + reader["empName"] + "City : " + reader["city"] + "<br />";
+            }
+            con.Close();
+        }
+    }
+}
