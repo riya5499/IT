5fda0a9891bf8399f125345d2bfa29a3a3b73474
riya5499 <33275363+riya5499@users.noreply.github.com>
2020-12-07 00:52:48 +0530

    Add files via upload

---
+++ Week 7/q3/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="q3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" ID="initial"></asp:Label>
+            <br />
+            <br />
+            Enter new value for Vanilla : <asp:TextBox runat="server" ID="newPrice"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button runat="server" ID="modify" Text="Modify" OnClick="ModifyAction"/>
+            <br />
+            <br />
+            
+            <asp:Label runat="server" ID="final"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week 7/q3/WebForm1.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace q3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string connectionString = WebConfigurationManager.ConnectionStrings["Product"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("SELECT * FROM Items WHERE flavour = 'Vanilla'", con);
+                try
+                {
+                    con.Open();
+                    SqlDataReader reader;
+                    reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        initial.Text = "Flavour : " + reader["flavour"].ToString() + "<br />" +
+                                        "Price : " + reader["price"].ToString();
+                    }
+                }
+                catch(Exception ex)
+                { }
+                con.Close();
+            }
+        }
+
+        protected void ModifyAction(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("UPDATE Items SET price = @p WHERE flavour = 'Vanilla'",con);
+            int p;
+            Int32.TryParse(newPrice.Text, out p);
+            command.Parameters.AddWithValue("@p", p);
+            try
+            {
+                con.Open();
+                command.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            { }
+            finally
+            {
+                con.Close();
+            }
+
+            con = new SqlConnection(connectionString);
+            command = new SqlCommand("SELECT * FROM Items WHERE flavour = 'Vanilla'", con);
+            try
+            {
+                con.Open();
+                SqlDataReader reader;
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    final.Text = "After Modification <br />" + "Flavour : " + reader["flavour"].ToString() + "<br />" +
+                                    "Price : " + reader["price"].ToString();
+                }
+            }
+            catch (Exception ex)
+            { }
+            con.Close();
+        }
+    }
+}
